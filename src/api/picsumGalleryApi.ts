a7a6176c0b149c24f652c0df00b254b44e732aef
Sunita-Gurau <sunitagurau.1999@gmail.com>
2024-11-18 15:29:45 +0545

    slicing of gallery layout and  handle pagesize and page limit logic

--- src/api/picsumGalleryApi.ts
+++ src/api/picsumGalleryApi.ts
@@ -2,5 +2,5 @@ import { PICSUM_IMAGE_LIST } from '../api/apiConstant'
 import { createApiConfig } from '../api/apiHelper'
 import type { AxiosRequestConfig } from 'axios'
 
-export const getPicsumImageList = (): AxiosRequestConfig =>
-  createApiConfig(PICSUM_IMAGE_LIST, 'get', undefined, undefined)
+export const getPicsumImageList = (params: { page: number; limit: number }): AxiosRequestConfig =>
+  createApiConfig(PICSUM_IMAGE_LIST, 'get', params, undefined)
--- src/views/PicsumGallery.vue
+++ src/views/PicsumGallery.vue
@@ -2,20 +2,61 @@
   <main>
     <div class="w-full h-screen">
       <h1>Picsum Gallery</h1>
-      <PaginationBar
-        class="pt-3"
-        :total-items="100"
-        :items-per-page="10"
-        :current-page="1"
-      ></PaginationBar>
+      <div class="max-w-[80px]">
+        <PgSelect
+          v-model="filterParams.limit"
+          placeholder="Status"
+          name="pageSize"
+          gap="mt-2"
+          height="h-7"
+          selectType="normalSelect"
+          optionSelected="objectLabelEmitted"
+          :options="pageSizeOptions"
+          @selected-option="handlePageSizeFilter"
+        />
+      </div>
+
+      <div
+        class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4"
+      >
+        <figure
+          v-for="image in picsumImageList"
+          :key="image.id"
+          class="relative group overflow-hidden cursor-pointer"
+        >
+          <img
+            :src="image.download_url"
+            :alt="`Image by ${image.author}`"
+            class="w-full h-full group-hover:scale-110 group transition-all duration-500"
+          />
+          <figcaption
+            class="flex w-full p-3 absolute -bottom-20 left-0 bg-slate-900/60 text-white justify-between items-center invisible group-hover:bottom-0 group-hover:visible transition-all duration-500"
+          >
+            <div class="flex flex-col gap-y-2">
+              <p class="text-lg font-semibold">{{ image.author }}</p>
+            </div>
+          </figcaption>
+        </figure>
+      </div>
+      <div class="flex justify-center items-center">
+        <PaginationBar
+          class="py-6"
+          :total-items="totalImages"
+          :items-per-page="filterParams.limit"
+          :current-page="filterParams.page"
+          @handle-page-changed="handlePageNumber"
+        ></PaginationBar>
+      </div>
     </div>
   </main>
 </template>
 <script setup lang="ts">
-import { onBeforeMount, ref } from 'vue'
+import { onBeforeMount, ref, watch, watchEffect } from 'vue'
 import { getPicsumImageList } from '../api/picsumGalleryApi'
 import { makeApiRequest } from '../api/apiHelper'
 import PaginationBar from '@/components/PaginationBar.vue'
+import PgSelect from '@/components/PgSelect.vue'
+import { useRoute, useRouter } from 'vue-router'
 
 interface PicsumImageType {
   id: string
@@ -25,11 +66,46 @@ interface PicsumImageType {
   url: string
   download_url: string
 }
+interface filterParamsType {
+  limit: number
+  page: number
+}
+interface PageSizeOption {
+  id: number
+  label: string
+}
 
+const totalImages = 1000
+const $route = useRoute()
+const $router = useRouter()
+const pageSizeOptions = ref<PageSizeOption[]>([
+  { id: 100, label: '100' },
+  { id: 200, label: '200' },
+  { id: 300, label: '300' },
+  { id: 400, label: '400' },
+  { id: 500, label: '500' },
+])
 const picsumImageList = ref<PicsumImageType[]>([])
+const filterParams = ref<filterParamsType>({
+  limit: 100,
+  page: 1,
+})
+
+const handlePageSizeFilter = (option: { id: number; label: string }) => {
+  filterParams.value.limit = Number(option.id)
+  filterParams.value.page = 1
+}
+
+const handlePageNumber = (page: number) => {
+  filterParams.value.page = page
+}
 
 const fetchPicsumImageList = () => {
-  makeApiRequest<PicsumImageType[]>(getPicsumImageList())
+  const params = {
+    page: filterParams.value.page,
+    limit: filterParams.value.limit,
+  }
+  makeApiRequest<PicsumImageType[]>(getPicsumImageList(params))
     .then((response) => {
       picsumImageList.value = response
     })
@@ -37,6 +113,35 @@ const fetchPicsumImageList = () => {
       console.log(error, 'error')
     })
 }
+
+watch(
+  () => filterParams.value,
+  () => {
+    $router.push({
+      name: 'picsumGallery',
+      query: filterParams.value,
+    })
+    fetchPicsumImageList()
+  },
+  { deep: true },
+)
+
+watchEffect(() => {
+  if ($route.query.page && $route.query.limit) {
+    const pageNumber = Number($route.query.page)
+    const pageSize = Number($route.query.limit)
+
+    if (!isNaN(pageNumber) && !isNaN(pageSize)) {
+      filterParams.value.page = pageNumber
+      filterParams.value.limit = pageSize
+    }
+  } else {
+    $router.push({
+      name: 'picsumGallery',
+      query: filterParams.value,
+    })
+  }
+})
 
 onBeforeMount(() => {
   fetchPicsumImageList()
